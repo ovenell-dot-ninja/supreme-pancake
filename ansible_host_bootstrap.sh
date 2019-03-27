b7bd076d68c01505ea949ee04b8b1d767341749c
fresh <fresh@canttouchthis.com>
2019-03-26 23:18:14 -0700

    push

---
+++ ansible_host_bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+echo " #  #  ##  #### #### #### "
+echo " # #  #  #    # #  # #  # "
+echo " ##   ####  ##  #  # #  # "
+echo " # #  #  # #    #  # #  # "
+echo " #  # #  # #### #### #### "
+echo "    Ansible  Bootstrap    "
+echo ""
+
+echo "Installing Prerequisites"
+sudo yum install -y git ansible
+
+echo "Cloning kazoo-ansible Repository"
+git clone https://github.com/kazoo-ansible/kazoo-ansible ~/kazoo-ansible
+
+echo "Installing kazoo-ansible Roles"
+sudo ansible-galaxy install -r ~/kazoo-ansible/requirements.yml
+
+if [ -e ~/.ssh/id_rsa -a -e ~/.ssh/id_rsa.pub ]
+then
+    echo "SSH keypair exists. Skipping..."
+else
+    echo "Generating SSH keypair"
+    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+fi
+
+cd ~/kazoo-ansible
+
